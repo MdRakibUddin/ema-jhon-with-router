83e4fa019f857ace7633ab86fcf34735ac3bb8f9
MdRakibUddin <rakibuddinctg25@gmail.com>
2021-10-07 22:49:00 +0600

    final commit

---
+++ src/components/PlaceOrder/PlaceOrder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import img from '../../images/giphy.gif';
+
+const PlaceOrder = () => {
+    return (
+        <div>
+            <img src={img} alt=""></img>
+
+        </div>
+    );
+};
+
+export default PlaceOrder;
